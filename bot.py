7fad51fba401030d01ded75e9fb765028ea38daf
rahmatullakhujaev <r.xo'jayev@student.inha.uz>
2024-07-23 21:26:27 +0500

    changed to ru-en

--- bot.py
+++ bot.py
@@ -7,31 +7,31 @@ from googletrans import Translator
 
 translator = Translator()
 
-def get_def(word_id):
-
-
-    app_id = "6d5e2af8"
-    app_key = "8bfa97e278b5c65703b9b608b8cbb95a"
-
-    language = "en-gb"
-    url = "https://od-api-sandbox.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
-    r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
-    res = r.json()
-
-    senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
-    definitions = []
-    for sense in senses:
-        definitions.append(f'👉{sense['definitions'][0]}')
-    definitions = '\n'.join(definitions)
-    if res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0].get('audioFile'):
-        audio = res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
-    else:
-        audio = None
-    output = {
-        "definitions": definitions,
-        "audio": audio
-    }
-    return output
+# def get_def(word_id):
+#
+#
+#     app_id = "6d5e2af8"
+#     app_key = "8bfa97e278b5c65703b9b608b8cbb95a"
+#
+#     language = "en-gb"
+#     url = "https://od-api-sandbox.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
+#     r = requests.get(url, headers={"app_id": app_id, "app_key": app_key})
+#     res = r.json()
+#
+#     senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
+#     definitions = []
+#     for sense in senses:
+#         definitions.append(f'👉{sense['definitions'][0]}')
+#     definitions = '\n'.join(definitions)
+#     if res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0].get('audioFile'):
+#         audio = res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
+#     else:
+#         audio = None
+#     output = {
+#         "definitions": definitions,
+#         "audio": audio
+#     }
+#     return output
 
 
 
@@ -50,32 +50,36 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm Dictionary bot!")
+    await message.reply("Write anything!\nНапишите что-нибудь")
 
 
 
 @dp.message_handler()
 async def send_def(message: types.Message):
+    # lang = translator.detect(message.text).lang
+    # if len(message.text.split()) >= 2 or not message.text.startswith('a'):
+    #     dest = 'uz' if lang == 'en' else 'en'
+    #     await message.reply(translator.translate(message.text, dest).text)
+    # else:
+    #     if lang == 'en':
+    #         word_id = message.text
+    #     else:
+    #         word_id = translator.translate(message.text, 'en').text
+    #
+    #     lookup = get_def(word_id)
+    #     if lookup.get('definitions'):
+    #         await message.reply(f'Word: {word_id}\nDefinition: {lookup["definitions"]}')
+    #         if lookup.get('audio'):
+    #             await message.reply_voice(lookup["audio"])
+    #     else:
+    #         if lang == 'en':
+    #             await message.reply('Word not found')
+    #         else:
+    #             await message.reply('So\'z topilmadi')
     lang = translator.detect(message.text).lang
-    if len(message.text.split()) >= 2 or not message.text.startswith('a'):
-        dest = 'uz' if lang == 'en' else 'en'
-        await message.reply(translator.translate(message.text, dest).text)
-    else:
-        if lang == 'en':
-            word_id = message.text
-        else:
-            word_id = translator.translate(message.text, 'en').text
+    dest = 'uz' if lang == 'en' else 'en'
+    await message.reply(translator.translate(message.text, dest).text)
 
-        lookup = get_def(word_id)
-        if lookup.get('definitions'):
-            await message.reply(f'Word: {word_id}\nDefinition: {lookup["definitions"]}')
-            if lookup.get('audio'):
-                await message.reply_voice(lookup["audio"])
-        else:
-            if lang == 'en':
-                await message.reply('Word not found')
-            else:
-                await message.reply('So\'z topilmadi')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
